8dbd50e4d44f54d2e0e36a377592bdde1986e9cf
Jeremy Maniece <developer@mrjvm.com>
2019-09-17 20:01:58 -0400

    20190917

---
+++ DrawingAShape/src/DrawingAShape.java
@@ -0,0 +1,9 @@
+public class DrawingAShape {
+	
+	public static void main(String[] args) {
+		System.out.println("   /|");
+		System.out.println("  / |");
+		System.out.println(" /  |");
+		System.out.println("/___|");
+	}
+}
